e00027f9266ebbfe6ff79cf25197f63fc8b12dac
Caio Miranda <caio.miranda@vsoft.com.br>
2025-01-31 11:35:03 -0300

    feat: adicionar connection string do banco de dados remoto

--- src/Tourmine.Users.Infrastructure/Settings.cs
+++ src/Tourmine.Users.Infrastructure/Settings.cs
@@ -2,6 +2,7 @@
 {
     public abstract class Settings
     {
-        public static string ConnectionString = Environment.GetEnvironmentVariable("tourmine-user-database") ?? string.Empty;
+        public static string ConnectionString = "Host=roundhouse.proxy.rlwy.net;Port=52938;Username=postgres;Password=bJqzJtfRLNlnjUwOHAAhNDUagGsLETvi;Database=railway"
+            ?? string.Empty;
     }
 }
