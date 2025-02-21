d09ea8488e9148cef6c3bb2cee9d9b7cfe427dee
ClodsNegreiros <clodsn@gmail.com>
2025-02-21 12:58:29 -0300

    feat: implementada DomainExceptionValidation para exceções personalizadas no domain

--- src/Tourmine.Users.Domain/Entities/User.cs
+++ src/Tourmine.Users.Domain/Entities/User.cs
@@ -1,4 +1,5 @@
 ﻿using Tourmine.Users.Domain.Enums;
+using Tourmine.Users.Domain.Validation;
 
 namespace Tourmine.Users.Domain.Entities
 {
@@ -8,5 +9,30 @@ namespace Tourmine.Users.Domain.Entities
         public string Email { get; set; }
         public string Password { get; set; }
         public EUserType UserType { get; set; }
+
+        public User(string name, string email, string password, EUserType userType)
+        {
+            ValidateDomain(name, email, password);
+            UserType = userType;
+        }
+
+        private void ValidateDomain(string name, string email, string password)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), 
+                "Invalid name. Name is required.");
+
+            DomainExceptionValidation.When(string.IsNullOrEmpty(email),
+                "Invalid email. Email is required.");
+
+            DomainExceptionValidation.When(string.IsNullOrEmpty(password),
+                "Invalid password. Password is required.");
+
+            DomainExceptionValidation.When(password.Length < 6,
+                "Invalid password. Password is too short.");
+
+            Name = name;
+            Email = email;
+            Password = password;
+        }
     }
 }
